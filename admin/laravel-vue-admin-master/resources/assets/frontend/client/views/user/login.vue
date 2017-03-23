69beb0d474a41b665b7a4fc11ed179007ff27434
kindziora <kindziora@live.de>
2017-03-24 00:02:50 +0100

    ajax login

---
+++ admin/laravel-vue-admin-master/resources/assets/frontend/client/views/user/login.vue
@@ -0,0 +1,63 @@
+<template>
+  <form action="/login" method="POST">
+    <div class="tile is-parent is-6 center-by-margin">
+      <article class="tile is-child box">
+        <h1 class="title">Hallo!</h1>
+        <h2 class="subtitle">Gleich gehts los</h2>
+        <div class="block">
+          <p class="control has-icon">
+            <input class="input" type="email" name="email" placeholder="E-Mail">
+            <span class="icon is-small">
+                    <i class="fa fa-envelope"></i>
+                  </span>
+          </p>
+          <p class="control has-icon">
+            <input class="input" type="password" name="password" placeholder="Password">
+            <span class="icon is-small">
+                    <i class="fa fa-lock"></i>
+                  </span>
+          </p>
+          <p class="control">
+            <button class="button is-success" v-on:click.prevent="onSubmit">
+                    Login
+                  </button>
+          </p>
+        </div>
+      </article>
+    </div>
+  </form>
+</template>
+
+<script>
+  import FormData from 'form-data'
+  export default {
+    computed: {
+      form_method: function () {
+        return this.$el.method.toLowerCase()
+      },
+      form_action: function () {
+        return this.$el.action.toLowerCase()
+      }
+    },
+    methods: {
+      onSubmit: function (event) {
+        var formData = new FormData(this.$el)
+        this.$http[this.form_method](this.form_action, formData)
+          .then(this.successCallBack, this.errorCallBack)
+      },
+      successCallBack: function (response) {
+        console.log('AjaxForm submission: SUCCESS')
+      },
+      errorCallBack: function (response) {
+        console.log('AjaxForm submission: ERROR')
+      }
+    }
+  }
+</script>
+
+<style>
+  .center-by-margin {
+    margin-left: auto;
+    margin-right: auto;
+  }
+</style>
