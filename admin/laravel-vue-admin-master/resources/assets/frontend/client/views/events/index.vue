66356b240e89c46040f665fdaeac786acd723d09
kindziora <kindziora@live.de>
2017-10-01 16:39:27 +0000

    load events on scroll

--- admin/laravel-vue-admin-master/resources/assets/frontend/client/views/events/index.vue
+++ admin/laravel-vue-admin-master/resources/assets/frontend/client/views/events/index.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
+    <select v-model="location" @change="changeFilter()">
+      <option value="story">Story</option>
+      <option value="poll">Poll</option>
+      <option value="show_hn">Show hn</option>
+      <option value="ask_hn">Ask hn</option>
+      <option value="front_page">Front page</option>
+    </select>
     <div class="tile is-ancestor" v-model="this.locations">
       <div class="tile is-parent" v-for="p in locations" :value="p">
         <router-link :to="{ path: 'events/' + p.id }">
@@ -20,24 +27,35 @@
         </router-link>
       </div>
     </div>
+   <infinite-loading @infinite="loadData" ref="infiniteLoading">
+      <span slot="no-more">
+        alle events geladen!
+      </span>
+    </infinite-loading>
   </div>
 </template>
 
 <script>
-  import Chart from 'vue-bulma-chartjs'
   import router from 'vue-router'
+  import InfiniteLoading from 'vue-infinite-loading';
   
   const api = '/events/list'
   
   export default {
     components: {
-      Chart,
-      router
+      router,
+      InfiniteLoading
     },
     data() {
       return {
         params: {},
-        locations: [],
+        locations: function(me) {
+          return [];
+          
+          let items = localStorage.getItem(router.fullPath)
+          return (items)?JSON.parse(items):[] 
+        }(this),
+        location : '',
         labels: [],
         isloading: false,
         user: JSON.parse(localStorage.getItem('me'))
@@ -45,8 +63,14 @@
     },
   
     methods: {
-      loadData() {
-        this.isloading = true
+      changeFilter() {
+        this.locations = [];
+        this.$nextTick(() => {
+          this.$refs.infiniteLoading.$emit('$InfiniteLoading:reset');
+        });
+      },
+      loadData($state) {
+        $state.isloading = true
         let name = this.$route.params.name;
         
         if(name === "me") {
@@ -54,30 +78,45 @@
            router.push({ path: '/events/user/' + name})
         }
         
+        this.reqData = {
+          user: name,
+          latlng : {},
+          location : '',
+          page: this.locations.length / 5 + 1
+        }
+        
         this.$http({
           url: api,
           transformResponse: [(data) => {
             return JSON.parse(data)
           }],
-          params: {
-            user: name,
-            latlng : {}
-          }
+          params: this.reqData
         }).then((response) => {
-          this.locations = response.data.data
-  
-          for (var i in this.locations) {
-            this.locations[i].images = JSON.parse(this.locations[i].images)
-          }
-          this.isloading = false
-  
+
+            if (response.data.data.length) {
+              
+              this.locations = this.locations.concat(response.data.data);
+              
+              for (var i in this.locations) {
+                this.locations[i].images = typeof this.locations[i].images === "string"? JSON.parse(this.locations[i].images):this.locations[i].images
+              }
+              $state.isloading = false
+              localStorage.setItem(router.fullPath, JSON.stringify(this.locations))
+              $state.loaded();
+              if (this.locations.length === response.data.total) {
+                $state.complete();
+              }
+            } else {
+              $state.complete();
+            } 
+            
         }).catch((error) => {
           console.log(error)
         })
       }
     },
     mounted() {
-      this.loadData()
+      //this.loadData()
     }
   }
 </script>
@@ -85,7 +124,7 @@
 <style scoped>
   .smallinfo{
     font-size:12px;
-    
+    background-color:lightgray;
     float: right;
   }
 </style>
